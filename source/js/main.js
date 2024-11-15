04a52a921544984018244b914dd4e0136e1925f2
booblik2022 <booblik2022@gmail.com>
2024-11-15 22:48:03 +0300

    Подключил tabs

--- source/js/main.js
+++ source/js/main.js
@@ -2,3 +2,11 @@
 // import Swiper from "swiper";
 // import {Navigation, Pagination} from "swiper/modules";
 // import 'swiper/css';
+
+import { initTabs } from './modules/tabs/init-tabs';
+
+window.addEventListener('DOMContentLoaded', () => {
+  window.addEventListener('load', () => {
+    initTabs();
+  });
+});
