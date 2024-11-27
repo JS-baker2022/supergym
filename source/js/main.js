92c7016b48081d25ea13c5d816ffc10546161439
booblik2022 <booblik2022@gmail.com>
2024-11-27 17:41:52 +0300

    Комит без названия

--- source/js/main.js
+++ source/js/main.js
@@ -1,12 +1,18 @@
 // https://swiperjs.com/get-started#installation
-// import Swiper from "swiper";
+//import Swiper from 'swiper';
 // import {Navigation, Pagination} from "swiper/modules";
 // import 'swiper/css';
 
-import { initTabs } from './modules/tabs/init-tabs';
+import { initTabs } from './vendor/tabs/init-tabs';
+import { initAccordions } from './vendor/accordion/init-accordion';
+import { initReviewsSwiper } from './modules/swiper/reviews';
+import { initVideo } from './modules/video';
 
 window.addEventListener('DOMContentLoaded', () => {
   window.addEventListener('load', () => {
     initTabs();
+    initAccordions();
+    initVideo();
+    initReviewsSwiper();
   });
 });
